94cdbaa5fbae34e767633fb5d9494d5333c17792
matthew.urffer <matthew.urffer@gmail.com>
2012-06-15 00:17:57 +0000

    Trying to use MCNP locally because of cluster problems

--- LayeredDetector/MCNP/GapMaterialStudy/RunData.sh
+++ LayeredDetector/MCNP/GapMaterialStudy/RunData.sh
@@ -4,7 +4,7 @@
 FILE='SCRIPT_GapMaterialStudyLayer_PS.mcnp'
 
 # Radius
-xposlog=(-1 -2 -4 -8 -16 -32 -64 -128)
+xposlog=(-2 -4 -8 -16 -32 -64)
 for x in ${xposlog[@]}
 do
 
@@ -12,6 +12,6 @@ do
     echo "Processing x position: $x" 
     cat $FILE | sed "s/GAMMAXPOS/$x/g"  > INP.mcnp
 
-    nohup mpirun -np 56 -machinefile NodeList /opt/MCNPX-2.60/bin/mcnpx ixr inp=INP.mcnp name=GammaFlux_SrcPosX_${x}.
+    nohup /opt/MCNPX-2.60/bin/mcnpx ixr inp=INP.mcnp name=GammaFlux_SrcPos_${x}.
 
 done

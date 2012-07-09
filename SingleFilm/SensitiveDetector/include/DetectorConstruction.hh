035501987c4038b17f52fd48678a8b41690cc81c
matthew.urffer <matthew.urffer@gmail.com>
2012-07-09 22:12:32 +0000

    Adding the Capability of DetectorMessenger.  Following novice/N03.

--- SingleFilm/SensitiveDetector/include/DetectorConstruction.hh
+++ SingleFilm/SensitiveDetector/include/DetectorConstruction.hh
@@ -14,54 +14,86 @@ class G4Material;
 
 class DetectorConstruction : public G4VUserDetectorConstruction{
 
-  public:
-    DetectorConstruction();
-    virtual ~DetectorConstruction();
+	public:
+		DetectorConstruction();
+		virtual ~DetectorConstruction();
 
-  public:
-    virtual G4VPhysicalVolume* Construct();
+		virtual G4VPhysicalVolume* Construct();
 
-     
-  private:
-    //! Define needed materials
-    void DefineMaterials();
-    //! Initialzie geometry parametners
-    void ComputeParameters();
-    //! Creates the Calorimeter (detector)
-    G4VPhysicalVolume* ConstructCalorimeter();
-    //! Sets the visualtiation attributes
-    void SetVisAttributes();
-    //! Sets the Sensitve Detectors
-    void SetSensitiveDetectors();
+		void PrintCaloParameters();
 
-    // Geometry Names
-    G4LogicalVolume* worldLV;
-    G4VPhysicalVolume* caloPV;
-	
-    // Materials
-    G4Material* defaultMaterial;    // Vacumun
-	G4Material* absorberMaterial;   // Detector material
-	G4Material* gapMaterial;        // Gap material
-	
-    // Geometry parameters
-    G4int nofLayers;                // Number of Layers
-	G4double absThickness;	        // Thickness of Absorber
-	G4double gapThickness;          // Thickness of Gap 
-	G4double outerRadius;		    // Outer Radius of Detector
-	G4double innerRadius;			// Inner radious of  Detector
-	G4double startAngle;
-	G4double spanAngle;
-	
-	G4double layerThickness;        // Thickness of a single layer
-	G4double caloThickness;         // Thickness of entire calorimeter
-    G4double worldSizeXY;
-    G4double worldSizeZ;
+		G4double GetWorldSizeX()			{return worldSizeX;};
+		G4double GetWorldSizeYZ()			{return worldSizeYZ;};
 
-    // Sensitive Detectors
-    CaloSensitiveDetector* caloSD;
+		G4double GetCaloThickness()		{return caloThickness;};
+		G4double GetCaloRadius()			{return outerRadius;};
 
-    // data members
-    G4bool  fCheckOverlaps; // option to activate checking of volumes overlaps
+		G4int GetNumberOfLayers()			{return nofLayers;};
+
+		G4Material* GetAbsorberMaterial()		{return absorberMaterial;};
+		G4double	 GetAbsorberThickness()			{return absThickness;};
+
+		G4Material* GetGapMaterial()				{return gapMaterial;};
+		G4double	 GetGapThickness()				{return gapThickness;};
+
+		const G4VPhysicalVolume* GetPhysicalWorld()		{return woldPV;};
+		const G4VPhysicalVolume* GetAbsorber()				{return absPV;};
+		const G4VPhysicalVolume* GetGap()					{return gapPV;};
+
+	private:
+		//! Define needed materials
+		void DefineMaterials();
+		//! Initialzie geometry parametners
+		void ComputeParameters();
+		//! Creates the Calorimeter (detector)
+		G4VPhysicalVolume* ConstructCalorimeter();
+		//! Sets the visualtiation attributes
+		void SetVisAttributes();
+		//! Sets the Sensitve Detectors
+		void SetSensitiveDetectors();
+
+		// Geometry Names
+		G4LogicalVolume* worldLV;			// Pointer to world logical volume
+		G4VPhysicalVolume* woldPV;			// Pointer to world physical volume
+
+		G4LogicalVolume* caloLV;			// Pointer to the calorimeter LV
+		G4VPhysicalVolume* caloPV;			// Pointer to the calorimater PV
+
+		G4LogicalVolume* layerLV;			// Pointer to a layer logical volume
+		G4VPhysicalVolume* layerPV;		// Pointer to a layer physical volume
+
+		G4LogicalVolume* absLV;				// Pointer to the absorber LV
+		G4VPhysicalVolume* absPV;			// Pointer to the absorber PV
+
+		G4LogicalVolume* gapLV;				// Pointer to the gap logical volume
+		G4VPhysicalVolume* gapPV;			// Pointer to the gap physical volume
+
+		// Materials
+		G4Material* defaultMaterial;    // Vacumun
+		G4Material* absorberMaterial;   // Detector material
+		G4Material* gapMaterial;        // Gap material
+
+		// Geometry parameters
+		G4int nofLayers;                // Number of Layers
+		G4double absThickness;	        // Thickness of Absorber
+		G4double gapThickness;          // Thickness of Gap 
+		G4double outerRadius;		    // Outer Radius of Detector
+		G4double innerRadius;			// Inner radious of  Detector
+		G4double startAngle;
+		G4double spanAngle;
+
+		G4double layerThickness;        // Thickness of a single layer
+		G4double caloThickness;         // Thickness of entire calorimeter
+		G4double worldSizeXY;
+		G4double worldSizeZ;
+
+		// Sensitive Detectors
+		CaloSensitiveDetector* caloSD;
+
+		// data members
+		G4bool  fCheckOverlaps; // option to activate checking of volumes overlaps
+
+		DetectorMessenger* detectorMessenger;		// Pointer to the messenger
 };
 
 #endif

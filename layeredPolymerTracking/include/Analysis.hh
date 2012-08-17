c00f6988c5b128e01d4214d69e0e34210bc9ebaf
matthew.urffer <matthew.urffer@gmail.com>
2012-08-17 20:22:09 +0000

    removing layeredPolymerTracking

--- layeredPolymerTracking/include/Analysis.hh
+++
@@ -1,9 +0,0 @@
-#ifndef Analysis_h
-#define Analysis_h 1
-
-#include "g4analysis_defs.hh"
-
-using namespace G4Root;
-//using namespace G4Xml;
-
-#endif
--- layeredPolymerTracking/svnDelUnRev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-svn status --no-ignore | grep '^\?' | sed 's/^\?      //'  | xargs -Ixx rm -rf xx

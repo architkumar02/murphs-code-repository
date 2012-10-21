8a8e3d3047499463a19806159ff779a593139a4d
matthew.urffer <matthew.urffer@gmail.com>
2012-10-21 20:52:17 +0000

    Working on genetic.c

--- CS-528/Project2-GP/genetic.c
+++ CS-528/Project2-GP/genetic.c
@@ -7,3 +7,24 @@
 #include "exprTree.h"
 
 void createForest(node && forest,int numTrees)
+void importData(char *filename, double *val, double *numPoints){
+
+    FILE *f = fopen("proj2-data.dat","r");
+    if (f){
+
+
+    }
+    else
+        fprintf(stderr,"Could not open %s for data import\n");
+
+
+}
+
+int main(int argc, char *argv[]){
+
+    /* Reading in the data file */
+    double val[][];
+    double numPoints;
+    importData("proj2-data.dat",&val,&numPoints);
+    return EXIT_SUCCESS;
+}

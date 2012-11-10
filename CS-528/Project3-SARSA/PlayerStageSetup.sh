a7c12532fd8d907d36843fe4d9af4b01f777fd3c
matthew.urffer <matthew.urffer@gmail.com>
2012-11-10 16:35:08 +0000

    Commit for Alan Nam

--- CS-528/Project3-SARSA/PlayerStageSetup.sh
+++ CS-528/Project3-SARSA/PlayerStageSetup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # Script to setup PlayerStage (On Hydra Machines)
+echo "Setting up Paths:"
 export PLAYERPATH="/user/bin"
 export STAGEPATH="/usr/bin;/usr/share/X11"
+echo "PLAYERPATH: $PLAYERPATH"
+echo "STAGEPATH: $STAGEPATH"
 
+echo "To run:"
+echo "[#] robot-player Project3.cfg"
+echo "[#] ./robotAgent"
 # To use Python Uncomment the following:
 #PY_VER=$(python -c "import sys; print '.'.join(map(str,sys.version_inf0[:2]))")
 #export PYTHONPATH="$PYTHONPATH:/usr/local/lib/python$PY_VER/site-packages"

b624f5ff6cf177185620f0ad5af0c0c9b2173b2d
matthew.urffer <matthew.urffer@gmail.com>
2012-11-28 18:24:27 +0000

    Working on detector design

---
+++ documentation/CompileLatex.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Compiles a latex with bibtext
+if [ $# -eq 0 ]
+then
+	echo "Need to specifiy file name or spellcheck"
+else
+	if [ "$1" ==  "spellcheck" ]
+	then
+		# Spell Check
+  		for f in *.tex;
+		do
+			aspell -t check f
+		done
+	else
+		version=$(svnversion .)
+      mkdir tmp								# For EPS to PDF files
+		cmd="pdflatex"
+		#options="--interaction=batchmode"
+		options=""
+		postProcess="grep Warning\|Overful"
+		pdflatex $options $1 | $postProcess 
+		bibtex $options $1 | $postProcess
+		pdflatex $options $1 | $postProcess
+		echo "All refrences should now be resolved"
+		echo
+		pdflatex $options $1 | $postProcess
+		#latex --interaction=batchmode $1 
+		#bibtex $1
+		#latex --interaction=batchmode $1
+		#latex --interaction=batchmode $1
+#		pdflatex $1
+      cp $1.pdf $1_v$version.pdf
+		# Clean up
+		rm -f *.vrb *.tdo *.out *.bbl *.blg *.dvi *.toc *.nav *.log *.aux *.snm
+		rm -rf tmp
+	fi
+fi

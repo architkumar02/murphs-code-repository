8ada1cd102fd6031fa348191b6259d6adcb75144
matthew.urffer <matthew.urffer@gmail.com>
2012-11-29 14:06:12 +0000

    Read for large scale graphs

--- CS-526/CallGraphChallenge/python/GraphUtility.py
+++ CS-526/CallGraphChallenge/python/GraphUtility.py
@@ -1,6 +1,6 @@
 import networkx as nx
-
 def readData(filename='LinkAnalyticsData/UTK_problem/Moria_1.graph'):
+    """ Creates a MultiGraph of the formated data supplied by filename """
     MG = nx.MultiGraph()
     with open(filename,'r') as f:
         for line in f:
@@ -15,3 +15,18 @@ def readData(filename='LinkAnalyticsData/UTK_problem/Moria_1.graph'):
                     secs=int(token[6]),                 # call duration
                     texts=long(token[7]))               # texts
     return MG
+                    
+def MultiToSingle(G,attr='calls'):
+    """ Converts a Multigraph to a single bidirectional weighted graph """
+    """ attr values are calls, secs, texts, days """
+    g = nx.Graph(weight=attr)
+    for u,v,edata in G.edges(data=True):
+        g.add_node(u)
+        g.add_node(v)
+        g.add_edge(u,v,weight=edata[attr])
+    return g
+
+def GetAttr(G,attr='calls'):
+    """ Returns a list of the of the values of the attribute in the graph"""
+    """ attr values are calls, secs, texts, days """
+    return [edata[attr] for u,v,edata in G.edges(data=True) if attr in edata]

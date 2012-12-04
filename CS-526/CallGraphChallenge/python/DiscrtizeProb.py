23e9c9d087af7131c3e5ee5c6f464f41b23e6ed5
matthew.urffer <matthew.urffer@gmail.com>
2012-12-04 16:58:00 +0000

    Running Analysis

--- CS-526/CallGraphChallenge/python/DiscrtizeProb.py
+++ CS-526/CallGraphChallenge/python/DiscrtizeProb.py
@@ -1,25 +1,6 @@
 import os
 import GraphUtility as GU
 import matplotlib.pyplot as pyplot
-def GetEdgeDistributions(dataPath='../LinkAnalyticsData/UTK_problem/'):
-    filenames=('Moria_1.graph','Standelf_1.graph')
-    attrs=('calls','texts','days','secs')
-    for f in filenames:
-        # Reading in the Graph
-        MG = GU.readData(os.path.join(dataPath,f))
-        # Distribution of each Attribute
-        for attr in attrs:
-            data = GU.GetAttr(MG,attr)
-            # Plotting
-            pyplot.hist(data,100)
-            pyplot.yscale('log')
-            pyplot.grid(True)
-            pyplot.ylabel("Frequency")
-            pyplot.xlabel(attr)
-            name = f.split('.')[0].split('_')[0]
-            title = name+" "+attr+" distribution"
-            pyplot.title(title)
-            pyplot.savefig(name+"_"+attr+"_distribution.png")
 
 import heapq
 import numpy as np
@@ -48,7 +29,6 @@ def GetDataDistributions(dataPath='../LinkAnalyticsData/UTK_problem/'):
             pyplot.savefig(name+"_"+attr+"_cum_distribution.png")
 
 def main():
-    GetEdgeDistributions()
     GetDataDistributions()
 
 if __name__ == "__main__":

eb9d0728dd209325d335defaf99d1904ac10fcd1
matthew.urffer <matthew.urffer@gmail.com>
2013-01-23 01:48:26 +0000

    Moderation of Cf-252 with HDPE

--- HDPEModeration/ProcessData.sh
+++ HDPEModeration/ProcessData.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-FILES=*.o
-echo "r energy tally total fraction"
+FILES=Output/*.o
+echo "r tally total"
 for f in $FILES
 do
     bf=$(basename $f .o)	
@@ -14,7 +14,7 @@ do
     F1=$(cat $f | sed -n "/1tally  21/,/1.0000E-07/p" | tail -1 | awk '{print $2 " " $3}' ) 
     F1total=$(cat $f | sed -n "/1tally  21/,/total/p" | tail -1 | awk '{print $2 " " $3}')
     
-    echo "$r $F1 $F1total $F2 $F2total"
+    echo "$r $F1 $F1total"
 
 # Cleanup
 done

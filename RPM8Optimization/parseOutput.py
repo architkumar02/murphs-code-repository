5fdf229ff05212e64cc65c776de06e24256b864b
matthew.urffer <matthew.urffer@gmail.com>
2013-02-11 18:20:59 +0000

    Hopefully have parseOutput workign

--- RPM8Optimization/parseOutput.py
+++ RPM8Optimization/parseOutput.py
@@ -93,6 +93,7 @@ def Write(data):
     for col, value in enumerate(summaryHeaders):
         summary.write(0,col,value)
     row = 1
+    print 'Starting to Write Summary'
     for d in data:
         summary.write(row,0,d['HDPE'])
         summary.write(row,1,d['NumLayers'])
@@ -100,11 +101,40 @@ def Write(data):
         summary.write(row,3,d['TallyTotal'][0])
         summary.write(row,4,d['TallyTotal'][1])
         row += 1
-
-    # Writing the data 
-    for d in data:
-        book.add_sheet(str.format('Mod_{0}_NumAssm_{1}_AssmSpace_{2}',d['HDPE'],d['NumLayers'],d['AssemblySpace']))
+    print 'Finsihed writing summary'
     
+    # Writing the data (grouping by numAssm}
+    row = 0
+    for d in data:
+        val = list()
+        val.append(d['HDPE']); del d['HDPE']
+        val.append(d['NumLayers']); del d['NumLayers']
+        val.append(d['AssemblySpace']); del d['AssemblySpace']
+        sheet = book.add_sheet(str.format('Mod_{0}_NumAssm_{1}_AssmSpace_{2}',val[0],val[1],val[2]))
+        # Writing
+        header=['Moderator (cm)','Num Assemblies','Space between assemblies','Tally','Cell','Reactions','Reactions Error']
+        for col, value in enumerate(header):
+            sheet.write(row,col,value)
+        row +=1
+        del d['TallyTotal']
+        for tkey in d:             # Dictionary with tallies as keys
+            for col, value in enumerate(val):
+                sheet.write(row,col,value)
+            col = len(val) 
+            sheet.write(row,col,tkey)
+            col +=1
+            t = d[tkey]
+            for c in t.keys():          
+                if c == 'total':
+                    pass
+                sheet.write(row,col,c)
+                col += 1
+                sheet.write(row,col,t[c][0])
+                col += 1
+                sheet.write(row,col,t[c][1])
+                col += 1
+            row +=1
+
     # Saving before exit
     book.save(str(date.today())+"Data.xls")
 

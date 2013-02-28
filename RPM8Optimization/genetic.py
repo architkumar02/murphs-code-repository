1900990f6ee517f4e6fa66190d334474a9019aeb
matthew.urffer <matthew.urffer@gmail.com>
2013-02-28 04:00:05 +0000

    Working on the genatic programing

---
+++ RPM8Optimization/genetic.py
@@ -0,0 +1,68 @@
+# A genetic algorthim approach to optimization of the RPM8
+import os
+import numpy as np
+from pyevolve import G1DBinaryString
+from pyevolve import G1DList
+from pyevolve import GSimpleGA
+from pyevolve.DBAdapters import DBFileCSV
+from pyevolve.GPopulation import GPopulation
+import geneticGeo as geo
+import geneticEval as evaluator
+
+
+def ParameterExplore():
+    g = G1DBinaryString.G1DBinaryString(13)
+    
+    # Two extremes
+    allMod = '0000000000000'
+    RunModel(allMod)
+    allDet = '1111111111111'
+    RunModel(allDet)
+
+    # Setting the first and last layer
+    for i in range(100):
+        g.initialize()
+        g[0] = 0
+        g[-1] = 0
+        RunModel(g.getBinary())
+
+    # Setting the first two and last two layers
+    for i in range(100):
+        g.initialize()
+        g[0] = 0
+        g[1] = 0
+        g[-1] = 0
+        g[-2] = 0
+        RunModel(g.getBinary())
+
+def evolve_callback(ga_engine):
+    generation = ga_engine.getCurrentGeneration()
+    pop = ga_engine.getPopulation()
+    # Getting a list of indvidviduals
+    indList = list()
+    for ind in pop:
+        indList.append(''.join(str(x) for x in ind[0:len(ind)]))
+    evaluator.runPopulation(indList)
+    return False
+
+def eval_func(chromosome):
+    if evaluator.countRate[chromosome] < 2.5:
+        return 0
+    else:
+        return evaluator.countRatePerMass[chromosome]
+    
+def main():
+    #ParameterExplore()
+    
+    evaluator.readOutput('CoarseRuns')
+    evaluator.printData()
+    csv_adapter = DBFileCSV(identify="run1",filename="stats_1.csv")
+    genome = G1DBinaryString.G1DBinaryString(13)
+    genome.evaluator.set(eval_func)
+    ga = GSimpleGA.GSimpleGA(genome)
+    ga.setDBAdapter(csv_adapter)
+    ga.stepCallback.set(evolve_callback)
+    ga.evolve(freq_stats=10)
+    
+if __name__ == "__main__":
+    main()
---
+++ RPM8Optimization/geneticEval.py
@@ -0,0 +1,93 @@
+# Evluation of a genome for genetic programs
+import os
+import numpy as np
+from multiprocessing import Process, Queue, Pool
+import geneticGeo as geo
+import MCNPXModel as mcnpx
+import mctal
+
+# Shared among all class instances
+interactions = dict()
+massLi = dict()
+countRate = dict()
+countRatePerMass = dict()
+    
+
+def calcResults(fname,massLiPerCell=7.30382E1*5.63475E-3,
+    sourceStrength = 2.3E3):
+    """ calcResults
+
+    Parses all of the output in a directory for the count per mg Li and count rate
+
+    Keywords:
+    fname -- filename
+    massLiPerCell -- mass of 6Li per cell # Print table 60 and 40 (2nd part)
+    sourceStrength -- strength of the source (n/s)
+
+    Return:
+    genome -- bitstring representation
+    """
+    genome = fname.split('OUT_')[1].strip('.m')
+    m = mctal.MCTAL(fname)
+    t = m.tallies[4]
+    inter = np.array([t.data[-1],t.errors[-1]])
+    m = genome.count('1')*massLiPerCell
+    cr = inter*sourceStrength
+     
+    interactions[genome] = inter
+    massLi[genome] = m
+    countRate[genome] = cr
+    countRatePerMass[genome] = cr/m
+    return genome
+
+def readOutput(path='CoarseRuns',massLiPerCell=7.30382E1*5.63475E-3,
+    sourceStrength = 2.3E3):
+    """ readOutput
+
+    Parses all of the output in a directory for the count per mg Li and count rate
+
+    Keywords:
+    path -- directory of output files
+    massLiPerCell -- mass of 6Li per cell # Print table 60 and 40 (2nd part)
+    sourceStrength -- strength of the source (n/s)
+
+    """
+    for files in os.listdir(path):
+        if files.endswith('.m'):
+            calcResults(os.path.join(path,files),massLiPerCell,sourceStrength)
+
+
+def runModel(genome,wait=False):
+    """ Runs the Model """
+    runName = 'INP_'+genome+'.mcnp'
+    g,num = geo.createGeometry(genome)
+    model = mcnpx.MCNPX(g,runName)
+    model.createInputDeck(g)
+    model.runModel()
+    if wait:
+        fname = model.NAME+'.m'
+        while not os.path.isfile(fname):
+            time.sleep(10)
+        calcResults(fname)
+
+def runPopulation(pop,wait=True):
+    """ Runs MCNPX on the population """
+    # What indicies to we have to run?
+    toRun = list()
+    for ind in pop:
+        if not ind in interactions:
+            toRun.append(ind)
+    # Running the pool
+    pool = Pool(processes=len(toRun))
+    for ind in toRun:
+        pool.apply_async(runModel,args=[ind,wait])
+    pool.close()
+    pool.join()  
+
+def printData(): 
+    with open('RunSummary.txt','w') as f:
+        f.write('Genome,Mass Li-6,Count Rate, Count Rate per Mass\n')
+        for g in countRate:
+            f.write(g+',{},{},{}\n'.format(massLi[g],
+            countRate[g][0],countRatePerMass[g][0]))
+           
